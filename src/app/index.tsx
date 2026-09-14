4ce82241aaa62cdb15b2c55fb9c7c43bf20a0b40
Carolina Costa <callinacosta@hotmail.com>
2026-09-14 10:23:44 -0300

    feat

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Edit src/app/index.tsx to edit this screen. Prof: Giovanni</Text>
     </View>
   );
 }
